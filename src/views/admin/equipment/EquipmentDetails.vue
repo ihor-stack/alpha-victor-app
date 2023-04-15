18b784de82390f4b0449d23592fcf6c6ff0e384f
YmayroFerreira <dev2@mythdigital.co>
2023-04-15 20:13:29 -0300

    equipment details adjustments

--- src/views/admin/equipment/EquipmentDetails.vue
+++ src/views/admin/equipment/EquipmentDetails.vue
@@ -29,6 +29,12 @@
         <ion-col size-xs="12" size-sm="6" class="form-admin--group_field">
           <DocumentField />
         </ion-col>
+        <ion-col size-xs="12" class="form-admin--group_field">
+          <ion-button>Add New Document+</ion-button>
+        </ion-col>
+        <ion-col size-xs="12" class="save-button">
+          <ion-button>Save Changes</ion-button>
+        </ion-col>
       </ion-row>
     </ion-grid>
 </template>
@@ -49,66 +55,13 @@
   import { ref } from "vue";
   import DocumentField from '@/components/admin/DocumentField.vue'
   
-  const manufacturers = ref([
-    { id: 1, name: 'opt 1' },
-    { id: 2, name: 'opt 2' },
-    { id: 3, name: 'opt 3' },
-  ]);
-  const assets = ref([
-    { id: 1, name: 'opt 1' },
-    { id: 2, name: 'opt 2' },
-    { id: 3, name: 'opt 3' },
-  ]);
-  const equipments = ref([
-    { id: 1, name: 'NUC7iDNKE', detail: 'intel.small.form.factor.pc' },
-    { id: 2, name: '1770342 USB Extender TX',  detail: 'extron.usb.extender'},
-    { id: 3, name: '65E8501', detail: 'sony.screen' },
-    { id: 4, name: '98 Inch QM98N', detail: 'samsung.screen' },
-  ]);
-  const documents = ref([
-    { id: 1, name: 'User Manual' },
-    { id: 2, name: 'Quick Start Guide' },
-    { id: 3, name: 'Schematics' },
-  ]);
-  const newDocument = ref()
-  const currentDocument = ref();
-  const editedDocument = ref();
 
-  const AddDocument = () => {
-    documents.value.push({id: documents.value.length + 1, name: newDocument.value})
-    newDocument.value = null
-  };
-  const SetCurrecntDocument = (obj:{id: number , name: string}) => {
-    currentDocument.value = obj
-  }
-  const RemoveDocument = (id: number) => {
-    documents.value = documents.value.filter(function( item ) {
-      return item.id !== id;
-    });
-  };
-  const SaveDocument = (id: number) => {
-    const index = documents.value.findIndex(item => item.id === id);
-    documents.value[index].name = editedDocument.value
-    currentDocument.value = null
-    editedDocument.value = null
-  }
-  const EnableEdit = (id: number) => {
-    const check = currentDocument?.value?.id === id ? true : false
-    return check
-  }
-  const ResetDocuments = (id: number) => {
-    const index = documents.value.findIndex(item => item.id === id);
-    documents.value[index] = currentDocument.value
-    currentDocument.value = null
-  };
 </script>
   
 <style scoped>
-  .remove-button {
-    --color: red;
-  }
   .save-button {
-    --color: green;
+    margin-top: 5%;
   }
+ 
 </style>
   
