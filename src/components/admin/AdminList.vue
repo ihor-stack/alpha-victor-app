dbc61acebdaba38100f3c7756f9d7d6e6aada363
YmayroFerreira <dev2@mythdigital.co>
2023-05-14 17:35:02 -0300

    created adminList component because ionpopover doesnt work in modals

---
+++ src/components/admin/AdminList.vue
@@ -0,0 +1,51 @@
+<template>
+    <ion-label v-if="label" color="light">
+      {{ label }}
+    </ion-label >
+    <ion-content>
+      <ion-list color="light" :value="modelValue.title">
+        <IonRadioGroup>
+          <ion-item 
+          color="light" 
+          v-for="(option) in props.options" 
+          :key="option.id" 
+          @click="$emit('update:modelValue', option)"
+          >
+            <ion-label >
+              {{option.title}} 
+            </ion-label>
+            <ion-radio slot="end" :value="option.title"/>
+          </ion-item>
+        </IonRadioGroup>
+      </ion-list>
+    </ion-content>
+  </template>
+  
+<script setup lang="ts">
+import { 
+  IonLabel,
+  IonList,
+  IonItem,
+  IonRadio,
+  IonRadioGroup,
+  IonContent
+} from "@ionic/vue";
+import {SelectItem} from '@/types/index'
+import { ref } from "vue";
+
+interface Props {
+  label?: string,
+  modelValue: SelectItem,
+  options: SelectItem[]
+  }
+  const props = defineProps<Props>();
+  const radioValue = ref()
+</script>
+  
+  <style scoped>
+  ion-content {
+    height: 150px;
+    background-color: white;
+  }
+  </style>
+  
