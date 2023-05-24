595d9dde5206ab7a7997d29b3e28f2bd43b2d957
myth-digital <dev1@mythdigital.co>
2023-05-24 21:27:20 +0400

    Update modal

--- src/components/modals/decisionTreeNodeModal/DecisionTreeNodeModal.vue
+++ src/components/modals/decisionTreeNodeModal/DecisionTreeNodeModal.vue
@@ -4,101 +4,58 @@
     @didDismiss="handleDismiss"
     :showBackdrop="false"
   >
-    <v-container v-if="state.step === 0">
-      <SelectArticle
-        v-if="editTreeNode?.type === 0"
-        :editTreeNode="editTreeNode"
-        :handleDismiss="() => handleDismiss()"
-        :handleClickConfirm="handleClickConfirm"
-        :handleClickBack="handleClickBack"
-      />
-      <SelectVideo
-        v-else-if="editTreeNode?.type === 1"
-        :editTreeNode="editTreeNode"
-        :handleDismiss="() => handleDismiss()"
-        :handleClickConfirm="handleClickConfirm"
-        :handleClickBack="handleClickBack"
-      />
-      <EditDestination
-        v-else-if="editTreeNode?.type === 2"
-        :editTreeNode="editTreeNode"
-        :type="editTreeNode?.type"
-        :handleDismiss="() => handleDismiss()"
-        :handleClickNext="handleClickNext"
-        :handleClickBack="handleClickBack"
-      />
-      <EditAnswer
-        v-else-if="editTreeNode?.type === 3"
-        :editTreeNode="editTreeNode"
-        :handleDismiss="() => handleDismiss()"
-        :handleClickConfirm="handleClickConfirm"
-      />
-      <AddEmail
-        v-else-if="editTreeNode?.type === 4"
-        :editTreeNode="editTreeNode"
-        :handleDismiss="() => handleDismiss()"
-        :handleClickConfirm="handleClickConfirm"
-        :handleClickBack="handleClickBack"
-      />
-      <AddPhone
-        v-else-if="editTreeNode?.type === 5"
-        :editTreeNode="editTreeNode"
-        :handleDismiss="() => handleDismiss()"
-        :handleClickConfirm="handleClickConfirm"
-        :handleClickBack="handleClickBack"
-      />
-      <SelectDocument
-        v-else-if="editTreeNode?.type === 6"
-        :editTreeNode="editTreeNode"
-        :handleDismiss="() => handleDismiss()"
-        :handleClickConfirm="handleClickConfirm"
-        :handleClickBack="handleClickBack"
-      />
-    </v-container>
-    <v-container v-else-if="state.step === 1">
-      <SelectArticle
-        v-if="editTreeNode?.type === 0"
-        :editTreeNode="editTreeNode"
-        :handleDismiss="() => handleDismiss()"
-        :handleClickConfirm="handleClickConfirm"
-        :handleClickBack="handleClickBack"
-      />
-      <SelectVideo
-        v-else-if="editTreeNode?.type === 1"
-        :editTreeNode="editTreeNode"
-        :handleDismiss="() => handleDismiss()"
-        :handleClickConfirm="handleClickConfirm"
-        :handleClickBack="handleClickBack"
-      />
-      <AddEmail
-        v-else-if="editTreeNode?.type === 4"
-        :editTreeNode="editTreeNode"
-        :handleDismiss="() => handleDismiss()"
-        :handleClickConfirm="handleClickConfirm"
-        :handleClickBack="handleClickBack"
-      />
-      <AddPhone
-        v-else-if="editTreeNode?.type === 5"
-        :editTreeNode="editTreeNode"
-        :handleDismiss="() => handleDismiss()"
-        :handleClickConfirm="handleClickConfirm"
-        :handleClickBack="handleClickBack"
-      />
-      <SelectDocument
-        v-else-if="editTreeNode?.type === 6"
-        :editTreeNode="editTreeNode"
-        :handleDismiss="() => handleDismiss()"
-        :handleClickConfirm="handleClickConfirm"
-        :handleClickBack="handleClickBack"
-      />
-    </v-container>
-    <EditDestination
-      v-else
+    <EditAnswer
+      v-if="editTreeNode?.type === 3"
       :editTreeNode="editTreeNode"
-      :type="editTreeNode.type"
+      :handleDismiss="() => handleDismiss()"
+      :handleClickConfirm="handleClickConfirm"
+      :handleClickBack="handleClickBack"
+    />
+    <EditDestination
+      v-else-if="state.step === 0"
+      :editTreeNode="editTreeNode"
+      :type="editTreeNode?.type"
       :handleDismiss="() => handleDismiss()"
       :handleClickNext="handleClickNext"
+      :handleClickBack="handleClickBack"
     />
+    <v-container v-else>
+      <SelectArticle
+        v-if="editTreeNode?.type === 0"
+        :editTreeNode="editTreeNode"
+        :handleDismiss="() => handleDismiss()"
+        :handleClickConfirm="handleClickConfirm"
+        :handleClickBack="handleClickBack"
+      />
+      <SelectVideo
+        v-else-if="editTreeNode?.type === 1"
+        :editTreeNode="editTreeNode"
+        :handleDismiss="() => handleDismiss()"
+        :handleClickConfirm="handleClickConfirm"
+        :handleClickBack="handleClickBack"
+      />
+      <AddEmail
+        v-else-if="editTreeNode?.type === 4"
+        :editTreeNode="editTreeNode"
+        :handleDismiss="() => handleDismiss()"
+        :handleClickConfirm="handleClickConfirm"
+        :handleClickBack="handleClickBack"
+      />
+      <AddPhone
+        v-else-if="editTreeNode?.type === 5"
+        :editTreeNode="editTreeNode"
+        :handleDismiss="() => handleDismiss()"
+        :handleClickConfirm="handleClickConfirm"
+        :handleClickBack="handleClickBack"
+      />
+      <SelectDocument
+        v-else-if="editTreeNode?.type === 6"
+        :editTreeNode="editTreeNode"
+        :handleDismiss="() => handleDismiss()"
+        :handleClickConfirm="handleClickConfirm"
+        :handleClickBack="handleClickBack"
+      />
+    </v-container>
   </ion-modal>
 </template>
 <script setup lang="ts">
@@ -153,6 +110,6 @@ const handleClickConfirm = (treeNodeData: ITreeNodeData) => {
 };
 
 const handleClickBack = () => {
-  state.step = 2;
+  state.step = 0;
 };
 </script>
