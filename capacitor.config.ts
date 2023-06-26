06c3ae99ca0d89164d3b349140e715a372c12f18
James Nesbitt <james@mythdigital.co>
2023-06-26 12:43:46 +0100

    Fix config issues with deploying iOS.

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -13,6 +13,19 @@ const config: CapacitorConfig = {
       resize: KeyboardResize.Native,
       resizeOnFullScreen: true
     },
+    "SplashScreen": {
+      "launchShowDuration": 2000,
+      "launchAutoHide": true,
+      "backgroundColor": "#ffffffff",
+      "androidScaleType": "CENTER_CROP",
+      "showSpinner": false,
+      "androidSpinnerStyle": "large",
+      "iosSpinnerStyle": "small",
+      "spinnerColor": "#999999",
+      "splashFullScreen": true,
+      "splashImmersive": true,
+      "layoutName": "launch_screen"
+    },
     BluetoothLe: {
       displayStrings: {
         scanning: "Scanning...",
@@ -22,10 +35,10 @@ const config: CapacitorConfig = {
       }
     }
   },
-  server: {
-    cleartext: true,
-    url: "http://192.168.1.76:8100"
-  }
+  // server: {
+  //   cleartext: true,
+  //   url: "http://192.168.1.76:8100"
+  // }
 };
 
 export default config;
